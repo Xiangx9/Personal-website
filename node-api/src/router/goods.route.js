afdd14914fd0f1b645ed0bc88deb206f63b6cf62
XX\xiang <878323885@qq.com>
2024-04-06 14:36:36 +0800

    一言

--- node-api/src/router/goods.route.js
+++ node-api/src/router/goods.route.js
@@ -1,7 +1,7 @@
-const Router = require('koa-router')
+const Router = require("koa-router");
 
-const { auth, hadAdminPermission } = require('../middleware/auth.middleware')
-const { validator } = require('../middleware/goods.middleware')
+const { auth, hadAdminPermission } = require("../middleware/auth.middleware");
+const { validator } = require("../middleware/goods.middleware");
 
 const {
   upload,
@@ -10,26 +10,26 @@ const {
   remove,
   restore,
   findAll,
-} = require('../controller/goods.controller')
+} = require("../controller/goods.controller");
 
-const router = new Router({ prefix: '/goods' })
+const router = new Router({ prefix: "/goods" });
 
 // 商品图片上传接口
-router.post('/upload', auth, hadAdminPermission, upload)
+router.post("/upload", auth, hadAdminPermission, upload);
 
 // 发布商品接口
-router.post('/', auth, hadAdminPermission, validator, create)
+router.post("/", auth, hadAdminPermission, validator, create);
 
 // 修改商品接口
-router.put('/:id', auth, hadAdminPermission, validator, update)
+router.put("/:id", auth, hadAdminPermission, validator, update);
 
 // 硬删除接口
 // router.delete('/:id', auth, hadAdminPermission, remove)
 
-router.post('/:id/off', auth, hadAdminPermission, remove)
-router.post('/:id/on', auth, hadAdminPermission, restore)
+router.post("/:id/off", auth, hadAdminPermission, remove);
+router.post("/:id/on", auth, hadAdminPermission, restore);
 
 // 获取商品列表
-router.get('/', findAll)
+router.get("/", findAll);
 
-module.exports = router
+module.exports = router;
